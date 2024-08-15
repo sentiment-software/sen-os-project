6dc192ff8dc4b863d4f4fb6c8515d32d0b1edd45
Sebők Bence <sebok.bence.work@gmail.com>
2024-08-15 02:29:27 +0200

    Add kernel welcome message test

--- src/main/boot/kernel.asm
+++ src/main/boot/kernel.asm
@@ -9,16 +9,45 @@ kernel_main:
   mov gs, ax
   mov ss, ax
 
-  mov edi, 0xb8000               ; Point to VRAM
-
-  ; Test me!
-  mov rax, 0x1F6C1F6C1F651F48
-  mov [edi], rax
-  mov rax, 0x1F6F1F571F201F6F
-  mov [edi + 8], rax
-  mov rax, 0x1F211F641F6C1F72
-  mov [edi + 16], rax
+  call clear_screen
+  call print_welcome_message
 
   .halt_kernel:
     hlt
     jmp .halt_kernel
+
+clear_screen:
+  push rax
+  push cx
+  mov edi, 0xb8000             ; Set VRAM address
+  mov rax, 0x0020002000200020  ; Set to black spaces
+  mov cx, 100                  ; Init counter
+  .clear_loop:
+    mov [edi], rax
+    add edi, 8
+    dec cx
+    jnz .clear_loop
+  pop cx
+  pop rax
+  ret
+
+print_welcome_message:
+    mov edi, 0xb8000               ; Point to VRAM
+    ; Test me!
+    mov rax, 0x1f631f6c1f651f57
+    mov [edi], rax
+    mov rax, 0x1f201f651f6d1f6f
+    mov [edi + 8], rax
+    mov rax, 0x1f6d1f201f6f1f74
+    mov [edi + 16], rax
+    mov rax, 0x1f2d1f661f201f79
+    mov [edi + 24], rax
+    mov rax, 0x1f6e1f691f6b1f63
+    mov [edi + 32], rax
+    mov rax, 0x1f651f6b1f201f67
+    mov [edi + 40], rax
+    mov rax, 0x1f6c1f651f6e1f72
+    mov [edi + 48], rax
+    mov rax, 0x1F001F001F001f21
+    mov [edi + 56], rax
+    ret
